fd853e445d9181022428d8e9ebe69806e1e33340
Breno Souza <brenosouzaemp@gmail.com>
2024-01-17 21:53:54 -0300

    Project created

---
+++ src/test/java/com/brenosouza/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenosouza.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
