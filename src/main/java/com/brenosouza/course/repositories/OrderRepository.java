dfca21fb3c725207dd030b16e6be1fa6d117ff02
Breno Souza <brenosouzaemp@gmail.com>
2024-01-20 20:05:47 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/brenosouza/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.brenosouza.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brenosouza.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/brenosouza/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.brenosouza.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brenosouza.course.entities.Order;
+import com.brenosouza.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
